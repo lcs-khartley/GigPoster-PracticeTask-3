fe81e6c2a492a7ea67491369c1972c6c87c4f1b5
Keira Hartley <khartley@lcs.on.ca>
2020-02-10 15:35:19 -0500

    Got all of the lines on the canvas using drawLine and loops

--- Gig Poster - Practice - 3.playground/Pages/Modest Mouse.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 3.playground/Pages/Modest Mouse.xcplaygroundpage/Contents.swift
@@ -36,29 +36,60 @@ let black = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
 canvas.fillColor = darkGrey
 canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 
-// Draw grey lines
+var yCoord = 0
+for y in stride(from: -400, through: 400, by: 40){
 
-var yCoord = 400
-
-canvas.lineColor = lightGrey
-
-for y in stride(from: 0, through: 400, by: 43){
-    
-    canvas.drawLine(from: Point(x: 0, y: y), to: Point(x: 400, y: yCoord), lineWidth: 15, capStyle: NSBezierPath.LineCapStyle.square)
+    if y <= 0 {
+        canvas.lineColor = black
+    }else{
+        canvas.lineColor = lightGrey
+    }
+canvas.drawLine(from: Point(x: 0, y: y), to: Point(x: 400, y: yCoord), lineWidth: 15, capStyle: NSBezierPath.LineCapStyle.square)
 
     yCoord += 40
-    
 }
 
-yCoord = 400
-canvas.lineColor = black
-for y in stride(from: 400, through: 0, by: 43){
-    
-    canvas.drawLine(from: Point(x: 0, y: y), to: Point(x: -400, y: yCoord), lineWidth: 15, capStyle: NSBezierPath.LineCapStyle.square)
+//// Draw grey lines
+//
+//var yCoord = 400
+//
+//canvas.lineColor = lightGrey
+//
+//for y in stride(from: 0, through: 400, by: 44){
+//
+//    canvas.drawLine(from: Point(x: 0, y: y), to: Point(x: 400, y: yCoord), lineWidth: 15, capStyle: NSBezierPath.LineCapStyle.square)
+//
+//    yCoord += 44
+//
+//}
+//
+//
+//// Draw black lines
+//yCoord = 400
+//
+//canvas.lineColor = black
+//for yTwo in stride(from: 0, through: -400, by: -44){
+//
+//    canvas.drawLine(from: Point(x: 0, y: yTwo), to: Point(x: 400, y: yCoord), lineWidth: 15, capStyle: NSBezierPath.LineCapStyle.square)
+//
+//    yCoord -= 44
+//
+//}
 
-    yCoord -= 40
+
+// Draw yellow lines
+canvas.lineColor = deepYellow
+
+yCoord = 360
+for yThree in stride(from: -40, through: 40, by: 40){
     
+    canvas.drawLine(from: Point(x: 0, y: yThree), to: Point(x: 400, y: yCoord), lineWidth: 16, capStyle: NSBezierPath.LineCapStyle.square)
+    
+    yCoord += 40
 }
+
+
+
 
 canvas.drawShapesWithBorders = false
 canvas.drawRectangle(at: Point(x: 0, y: 400), width: 400, height: 200)
